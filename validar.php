9f340c98392580496edbcb48337fe9dfb632c6b7
Gamezjona <gamezjona27@gmail.com>
2023-04-30 18:44:22 -0600

    Actualizado

---
+++ validar.php
@@ -0,0 +1,17 @@
+<?php
+include("conexion.php");
+
+$nombre = $_POST['nombre2'];
+$email = $_POST['email2'];
+$contraseña = $_POST['contraseña3'];
+$consulta2 = "SELECT nombreClinete FROM cliente WHERE nombreClinete = '$nombre' AND emailCliente = '$email' AND contraseña = '$contraseña'";
+$resultado2=mysqli_query($conexion,$consulta2);
+$row2 = mysqli_num_rows($resultado2);
+
+echo $row2;
+
+if($row2 > 0){
+    header("Location: inicio.html");
+}else{
+    header("Location: errorUsuario.html");
+}
